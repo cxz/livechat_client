2b30da3742822e3df9e05eb53d9598ca6ed12d3b
amaia <alexandre.maia@gmail.com>
2013-08-17 01:32:22 -0300

    added chats

--- lib/livechat/client/chats.rb
+++ lib/livechat/client/chats.rb
@@ -10,6 +10,10 @@ module LiveChat
       def initialize(client, *args)
         super(client, :chat, *args)
       end
+
+      def send_transcript(*args)
+        post("#{@query[:path]}/send_transcripts", Hash[*args])
+      end
     end
   end
 end
